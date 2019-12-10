5b76f1c796129681ee1677ae7ad66ce9488e7b7a
Bishoy <abdelmab@codescalers.com>
2019-12-10 15:36:27 +0200

    add virtualenv

--- flist.sh
+++ flist.sh
@@ -98,12 +98,14 @@ prepare_taiga_file='/opt/bin/prepare_taiga.sh'
 set -x
 # Install dependencies and populate database
 cd /home/taiga/taiga-back
+virtualenv -p /usr/bin/python3 taiga
 /home/taiga/taiga-back/taiga/bin/pip3 install -r requirements.txt
 /home/taiga/taiga-back/taiga/bin/python3 manage.py migrate --noinput
 /home/taiga/taiga-back/taiga/bin/python3 manage.py loaddata initial_user
 /home/taiga/taiga-back/taiga/bin/python3 manage.py loaddata initial_project_templates
 /home/taiga/taiga-back/taiga/bin/python3 manage.py compilemessages
 /home/taiga/taiga-back/taiga/bin/python3 manage.py collectstatic --noinput
+
 EOF
 
 tar -cpzf "/root/archives/circles.tar.gz" --exclude dev --exclude sys --exclude proc --exclude /root/archives/ /
