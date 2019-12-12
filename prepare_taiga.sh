0c99231e110568b026a14ebe01f17798b6ef9dd6
Bishoy <you@example.com>
2019-12-12 16:00:42 +0200

    fix sed command

--- prepare_taiga.sh
+++ prepare_taiga.sh
@@ -14,8 +14,8 @@ virtualenv -p /usr/bin/python3 taiga
 # edit backend
 sed -i "s|http://localhost/static/|https://$HOST_IP/static/|g"  /home/taiga/taiga-back/settings/local.py
 sed -i "s|http://localhost/media/|https://$HOST_IP/media/|g" /home/taiga/taiga-back/settings/local.py
-sed -i "s|'localhost'|"$HOST_IP"|g" /home/taiga/taiga-back/settings/local.py
-sed "s|'http'|'https'|g" /home/taiga/taiga-back/settings/local.py
+sed -i "s|'localhost'|\"$HOST_IP\"|g" /home/taiga/taiga-back/settings/local.py
+sed -i "s|'http'|'https'|g" /home/taiga/taiga-back/settings/local.py
 
 # Edit conf files for frontend
 sed -i "s|circles.threefold.me|$HOST_IP|g" /home/taiga/taiga-front-dist/dist/conf.json
