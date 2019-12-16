4c31c56aaafe414a11d3624b660cf5426d5a3dd5
Bishoy <abdelmab@codescalers.com>
2019-12-16 17:49:39 +0200

    add taiga home to be presistent

--- flist.sh
+++ flist.sh
@@ -21,88 +21,13 @@ go run build.go
 cp -p restic /usr/bin/restic
 rm -rf restic
 
-adduser taiga
-adduser taiga sudo
-passwd -d taiga
-
-cd /home/taiga
-git clone https://github.com/threefoldtech/Threefold-Circles.git taiga-back
-cd taiga-back
-git checkout production
-
-su taiga && cd /home/taiga && sudo virtualenv -p /usr/bin/python3 taiga
-
-local_file='/home/taiga/taiga-back/settings/local.py'
-/bin/cat <<EOF > $local_file
-from .common import *
-import os
-
-MEDIA_URL = "http://localhost/media/"
-STATIC_URL = "http://localhost/static/"
-SITES["front"]["scheme"] = 'http'
-SITES["front"]["domain"] = 'localhost'
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-DEBUG = False
-PUBLIC_REGISTER_ENABLED = True
-
-DEFAULT_FROM_EMAIL = "no-reply@example.com"
-SERVER_EMAIL = DEFAULT_FROM_EMAIL
-
-# CELERY_ENABLED = True
-
-EVENTS_PUSH_BACKEND = "taiga.events.backends.rabbitmq.EventsPushBackend"
-EVENTS_PUSH_BACKEND_OPTIONS = {"url": f"amqp://taiga:{SECRET_KEY}@localhost:5672/taiga"}
-
-# Uncomment and populate with proper connection parameters
-# for enable email sending. EMAIL_HOST_USER should end by @domain.tld
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_USE_TLS = False
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-# EMAIL_PORT = 25
-
-# Uncomment and populate with proper connection parameters
-# for enable github login/singin.
-# GITHUB_API_CLIENT_ID = "yourgithubclientid"
-# GITHUB_API_CLIENT_SECRET = "yourgithubclientsecret"
-EOF
-
-cd /home/taiga
-git clone https://github.com/threefoldtech/Threefold-Circles-front-dist.git taiga-front-dist
-cd taiga-front-dist
-git checkout production
-git pull
-
-taiga_front_conf='/home/taiga/taiga-front-dist/dist/conf.json'
-wget https://raw.githubusercontent.com/threefoldtech/Threefold-Circles-Flist/master/taiga-front-dist.conf -O $taiga_front_conf
-
-# Events installation
-
-cd /home/taiga
-git clone https://github.com/threefoldtech/Threefold-Circles-events.git taiga-events
-su taiga \
-&& cd taiga-events \
-&& git checkout master \
-&& git pull \
-&& curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - \
-&& sudo apt-get install -y nodejs \
-&& npm install \
-&& cp config.example.json config.json
-
 # configure nginx and startup toml
 
 rm /etc/nginx/sites-enabled/default
-mkdir -p /home/taiga/logs
 nginx_file='/etc/nginx/conf.d/taiga.conf'
 wget https://raw.githubusercontent.com/threefoldtech/Threefold-Circles-Flist/master/nginx_conf -O ${nginx_file}
 wget https://raw.githubusercontent.com/threefoldtech/Threefold-Circles-Flist/master/startup.toml -O /.startup.toml
 sudo nginx -t
-mkdir -p /opt/bin
-prepare_taiga_file='/opt/bin/prepare_taiga.sh'
-wget https://raw.githubusercontent.com/threefoldtech/Threefold-Circles-Flist/master/prepare_taiga.sh -O ${prepare_taiga_file}
 
 
 tar -cpzf "/root/archives/circles.tar.gz" --exclude dev --exclude sys --exclude proc --exclude /root/archives/ /
---
+++ setup_taiga.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -x
+
+if getent passwd taiga2; then
+    echo username taiga is already exist
+else
+    adduser taiga
+    adduser taiga sudo
+    passwd -d taiga
+fi
+
+if [ ! -d /home/taiga/taiga-back ] ; then
+    cd /home/taiga
+    git clone https://github.com/threefoldtech/Threefold-Circles.git taiga-back
+    cd taiga-back
+    git checkout production
+
+    su taiga && cd /home/taiga && sudo virtualenv -p /usr/bin/python3 taiga
+
+    local_file='/home/taiga/taiga-back/settings/local.py'
+    wget https://raw.githubusercontent.com/threefoldtech/Threefold-Circles-Flist/master/local.py -O $local_file
+else
+    echo taiga back dir is already exist
+
+fi
+
+if [ ! -d /home/taiga/taiga-front-dist ] ; then
+    cd /home/taiga
+    git clone https://github.com/threefoldtech/Threefold-Circles-front-dist.git taiga-front-dist
+    cd taiga-front-dist
+    git checkout production
+    git pull
+
+    taiga_front_conf='/home/taiga/taiga-front-dist/dist/conf.json'
+    wget https://raw.githubusercontent.com/threefoldtech/Threefold-Circles-Flist/master/taiga-front-dist.conf -O $taiga_front_conf
+
+else
+    echo taiga_front-dist is already exist
+
+fi
+# Events installation
+
+if [ ! -d /home/taiga/taiga-events ]; then
+    cd /home/taiga
+    git clone https://github.com/threefoldtech/Threefold-Circles-events.git taiga-events
+    su taiga \
+    && cd taiga-events \
+    && git checkout master \
+    && git pull \
+    && curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - \
+    && sudo apt-get install -y nodejs \
+    && npm install \
+    && cp config.example.json config.json
+    # add logs dir for taiga logs
+    mkdir -p /home/taiga/logs
+
+else
+    echo taiga-events is already exist
+fi
