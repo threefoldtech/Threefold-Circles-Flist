241d78eaac46eee254aefbba8ad7f0ea3388c892
Bishoy <abdelmab@codescalers.com>
2019-12-22 10:38:48 +0200

    fix error in setup tiaga back

--- prepare_taiga.sh
+++ prepare_taiga.sh
@@ -9,6 +9,16 @@ if [[ -z "$SECRET_KEY" ]] || [[ -z "$EMAIL_HOST" ]] || [[ -z "$EMAIL_HOST_USER" 
     exit 1
 fi
 
+# Install dependencies and populate database
+cd /home/taiga/taiga-back
+sudo virtualenv -p /usr/bin/python3 taiga
+/home/taiga/taiga-back/taiga/bin/pip3 install -r requirements.txt
+/home/taiga/taiga-back/taiga/bin/python3 manage.py migrate --noinput
+/home/taiga/taiga-back/taiga/bin/python3 manage.py loaddata initial_user
+/home/taiga/taiga-back/taiga/bin/python3 manage.py loaddata initial_project_templates
+/home/taiga/taiga-back/taiga/bin/python3 manage.py compilemessages
+/home/taiga/taiga-back/taiga/bin/python3 manage.py collectstatic --noinput
+
 # edit backend
 sed -i "s|http://localhost/static/|https://$HOST_IP/static/|g"  /home/taiga/taiga-back/settings/local.py
 sed -i "s|http://localhost/media/|https://$HOST_IP/media/|g" /home/taiga/taiga-back/settings/local.py
@@ -21,14 +31,3 @@ sed -i "s|circles.threefold.me|$HOST_IP|g" /home/taiga/taiga-front-dist/dist/con
 # Edit config.json for events
 sed -i "s/guest:guest/taiga:$SECRET_KEY/g" /home/taiga/taiga-events/config.json
 sed -i "s/mysecret/$SECRET_KEY/g" /home/taiga/taiga-events/config.json
-
-# Install dependencies and populate database
-cd /home/taiga/taiga-back
-virtualenv -p /usr/bin/python3 taiga
-/home/taiga/taiga-back/taiga/bin/pip3 install -r requirements.txt
-/home/taiga/taiga-back/taiga/bin/python3 manage.py migrate --noinput
-/home/taiga/taiga-back/taiga/bin/python3 manage.py loaddata initial_user
-/home/taiga/taiga-back/taiga/bin/python3 manage.py loaddata initial_project_templates
-/home/taiga/taiga-back/taiga/bin/python3 manage.py compilemessages
-/home/taiga/taiga-back/taiga/bin/python3 manage.py collectstatic --noinput
-
