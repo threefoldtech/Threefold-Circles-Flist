101c45d4a7ee6eca67e8d77656afa10e9eaa877f
Bishoy <abdelmab@codescalers.com>
2019-12-22 17:17:03 +0200

    fix login

--- setup_taiga.sh
+++ setup_taiga.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-# create a database user and taiga database 
+# create a database user and taiga database
 #sudo -u postgres createuser taiga
 su - postgres -c "psql -t -c '\du' | cut -d \| -f 1 | grep -qw taiga && echo taiga user already exist || createuser taiga"
 su - postgres -c "psql -lqt | cut -d \| -f 1 | grep -qw  taiga && echo taiga database is already exist || createdb taiga -O taiga --encoding='utf-8' --locale=en_US.utf8 --template=template0"
@@ -18,14 +18,14 @@ if [[ ! -d /home/taiga/taiga-back ]] ; then
     cd /home/taiga
     git clone https://github.com/threefoldtech/Threefold-Circles.git taiga-back
     cd taiga-back
-    git checkout master
+    git checkout production
     su taiga && cd /home/taiga && sudo virtualenv -p /usr/bin/python3 taiga
     local_file='/home/taiga/taiga-back/settings/local.py'
     wget https://raw.githubusercontent.com/threefoldtech/Threefold-Circles-Flist/master/local.py -O $local_file
     # Install dependencies and populate database
-    cd /home/taiga/taiga-back
-    virtualenv -p /usr/bin/python3 taiga
-    /home/taiga/taiga-back/taiga/bin/pip3 install -r requirements.txt
+ #   cd /home/taiga/taiga-back
+ #   virtualenv -p /usr/bin/python3 taiga
+ #   /home/taiga/taiga-back/taiga/bin/pip3 install -r requirements.txt
 else
     echo taiga back dir is already exist
 
