2670041443478726025c720b3f6cbc5a200b1a0a
Bishoy <abdelmab@codescalers.com>
2020-01-22 11:03:05 +0200

    fix

--- local.py
+++ local.py
@@ -6,8 +6,6 @@ STATIC_URL = "http://localhost/static/"
 SITES["front"]["scheme"] = 'http'
 SITES["front"]["domain"] = 'localhost'
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-
 DEBUG = False
 PUBLIC_REGISTER_ENABLED = True
 
@@ -17,16 +15,16 @@ SERVER_EMAIL = DEFAULT_FROM_EMAIL
 # CELERY_ENABLED = True
 
 EVENTS_PUSH_BACKEND = "taiga.events.backends.rabbitmq.EventsPushBackend"
-EVENTS_PUSH_BACKEND_OPTIONS = {"url": f"amqp://taiga:{SECRET_KEY}@localhost:5672/taiga"}
+EVENTS_PUSH_BACKEND_OPTIONS = {"url": f"amqp://taiga:$SECRET_KEY@localhost:5672/taiga"}
 
 # Uncomment and populate with proper connection parameters
 # for enable email sending. EMAIL_HOST_USER should end by @domain.tld
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_USE_TLS = False
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-# EMAIL_PORT = 25
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = "$EMAIL_HOST"
+EMAIL_HOST_USER = "$EMAIL_HOST_USER"
+EMAIL_HOST_PASSWORD = "$EMAIL_HOST_PASSWORD"
+EMAIL_PORT = 587
 
 # Uncomment and populate with proper connection parameters
 # for enable github login/singin.
